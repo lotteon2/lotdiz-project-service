d8b26a1ceec1f22b6ff8b0b98eb4dca68b4e8318
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2023-10-06 09:27:03 +0900

    fix: project entity jparepository test code

---
+++ src/main/java/com/lotdiz/projectservice/repository/BannerRepository.java
@@ -0,0 +1,6 @@
+package com.lotdiz.projectservice.repository;
+
+import com.lotdiz.projectservice.entity.Banner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BannerRepository extends JpaRepository<Banner, Long> {}
---
+++ src/main/java/com/lotdiz/projectservice/repository/MakerRepository.java
@@ -0,0 +1,6 @@
+package com.lotdiz.projectservice.repository;
+
+import com.lotdiz.projectservice.entity.Maker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MakerRepository extends JpaRepository<Maker, Long> {}
