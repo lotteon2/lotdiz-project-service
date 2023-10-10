60ee1ec33ee8a1120da69b1e7e0def91e5865b4a
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2023-10-10 23:32:46 +0900

    fix: funding, member feign endpoint

--- src/main/java/com/lotdiz/projectservice/client/FundingServiceClient.java
+++ src/main/java/com/lotdiz/projectservice/client/FundingServiceClient.java
@@ -4,19 +4,18 @@ import com.lotdiz.projectservice.dto.response.FundingAchievementResultOfProjectD
 import com.lotdiz.projectservice.dto.response.FundingAchievementResultOfProjectResponseDto;
 import com.lotdiz.projectservice.utils.SuccessResponse;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "fundingServiceClient", url = "https://952a1112-3483-413d-90a6-6befa9974329.mock.pstmn.io")
+@FeignClient(name = "fundingServiceClient", url = "${endpoint.funding-service}")
 public interface FundingServiceClient {
 
   @GetMapping("/projects/achievement")
-  SuccessResponse<HashMap<String, FundingAchievementResultOfProjectResponseDto>> getFundingOfProject(
-      @RequestParam List<Long> projects);
+  SuccessResponse<HashMap<String, FundingAchievementResultOfProjectResponseDto>>
+      getFundingOfProject(@RequestParam List<Long> projects);
 
   @GetMapping("/projects/{projectId}/achievement")
   SuccessResponse<FundingAchievementResultOfProjectDetailResponseDto> getFundingOfProjectDetail(
--- src/main/java/com/lotdiz/projectservice/client/MemberServiceClient.java
+++ src/main/java/com/lotdiz/projectservice/client/MemberServiceClient.java
@@ -7,15 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "member-service", url = "${endpoint.member-service}")
+@FeignClient(name = "memberServiceClient", url = "${endpoint.member-service}")
 public interface MemberServiceClient {
 
-  // TODO
   @GetMapping("/islike")
   SuccessResponse<Long> getIsLike(@RequestBody IsLikeOfMemberRequestDto isLikeOfMemberRequestDto);
 
-  // TODO
   @GetMapping("/projects/{projectId}/like-count")
   SuccessResponse<Long> getCountLike(@PathVariable Long projectId);
-
 }
