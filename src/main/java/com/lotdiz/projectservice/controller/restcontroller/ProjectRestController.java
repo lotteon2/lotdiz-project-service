51b13836058b4dff8ba99739b4dd2f612ba20a7f
SOYOUNG CHOI <66711073+cso6005@users.noreply.github.com>
2023-10-11 09:15:01 +0900

    refactor: add /api at rest api

--- src/main/java/com/lotdiz/projectservice/controller/restcontroller/ProjectRestController.java
+++ src/main/java/com/lotdiz/projectservice/controller/restcontroller/ProjectRestController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class ProjectRestController {
 
   private final ProjectForSupporterService projectForSupporterService;
