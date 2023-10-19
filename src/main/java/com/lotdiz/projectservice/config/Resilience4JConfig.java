e04fcda10f217c642b2d115f80048d46a60a2ef0
CokeLee777 <akak640@kyonggi.ac.kr>
2023-10-19 14:57:48 +0900

    refactor: refactoring resilience4j and projectduedate sqs listener logic
    - circuitbreaker 전략 추가 및 강제 캐스팅 제거, fallback 함수 추가
    - 프로젝트 마감 알림 로직 리팩터링

--- src/main/java/com/lotdiz/projectservice/config/Resilience4JConfig.java
+++ src/main/java/com/lotdiz/projectservice/config/Resilience4JConfig.java
@@ -17,13 +17,29 @@ public class Resilience4JConfig {
 
     CircuitBreakerConfig circuitBreakerConfig =
         CircuitBreakerConfig.custom()
-            .failureRateThreshold(25) // 100번중에 25번 실패할 경우 작동
-            .waitDurationInOpenState(Duration.ofMillis(1000)) // CircuitBreaker가 작동하면 1초동안 Open상태 유지
-            .slidingWindowType(
-                CircuitBreakerConfig.SlidingWindowType.COUNT_BASED) // 개수 기반으로 metric 수집
-            .slidingWindowSize(2) // 통계 큐 사이즈
+            // 요청 실패한 횟수를 기준으로 실패율 계산
+            .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+            // 요청의 응답 시간이 이 값 이상일 때 slow call 상태로 계산
+            .slowCallDurationThreshold(Duration.ofSeconds(3))
+            // slow call 요청의 비율이 (이 값)% 이상이 되면 회로 차단(Open state)
+            .slowCallRateThreshold(80f)
+            // 요청 실패 비율이 (이 값)% 이상이 되면 회로 차단(Open state)
+            .failureRateThreshold(80f)
+            // 요청의 실패와 slow call의 비율을 계산할 최소 요청 수 지정
+            .minimumNumberOfCalls(100)
+            // 회로가 닫혀있을 때 기록할 슬라이딩 윈도우 개수
+            .slidingWindowSize(100)
+            // 회로가 반 개방 상태일 때 받아들일 요청의 개수
+            .permittedNumberOfCallsInHalfOpenState(100)
+            // 회로가 반 개방 상태일 때 열림 상태가 되기까지 대기시간
+            .maxWaitDurationInHalfOpenState(Duration.ofMillis(1000))
+            // 열린 상태로 이 시간만큼 유지 후 반 개방상태로 전환
+            .waitDurationInOpenState(Duration.ofMillis(1000))
+            .automaticTransitionFromOpenToHalfOpenEnabled(false)
+                .ignoreExceptions(ClassCastException.class)
             .build();
 
+    // future supplier의 time limit 지정: 4초 동안 응답이 오지 않으면 Open state
     TimeLimiterConfig timeLimiterConfig =
         TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build();
 
--- src/main/java/com/lotdiz/projectservice/mapper/ProjectMapper.java
+++ src/main/java/com/lotdiz/projectservice/mapper/ProjectMapper.java
@@ -1,5 +1,6 @@
 package com.lotdiz.projectservice.mapper;
 
+import com.lotdiz.projectservice.dto.request.GetTargetAmountCheckExceedRequestDto;
 import com.lotdiz.projectservice.dto.request.ProjectInformationForAchievedTargetAmountRequestDto;
 import com.lotdiz.projectservice.entity.Project;
 import java.util.List;
@@ -19,4 +20,13 @@ public interface ProjectMapper {
   @IterableMapping(qualifiedByName = "PIFATARD")
   List<ProjectInformationForAchievedTargetAmountRequestDto>
       getListOfProjectInformationForAchievedTargetAmountRequestDto(List<Project> projects);
+
+  @Named("TACERD")
+  @Mapping(target = "makerMemberId", source = "maker.memberId")
+  GetTargetAmountCheckExceedRequestDto projectToGetTargetAmountCheckExceedRequestDto(
+      Project project);
+
+  @IterableMapping(qualifiedByName = "TACERD")
+  List<GetTargetAmountCheckExceedRequestDto> projectsToGetTargetAmountCheckExceedRequestDtos(
+      List<Project> projects);
 }
