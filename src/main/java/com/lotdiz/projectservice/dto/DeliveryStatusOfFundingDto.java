d295d55bb72cefa00bb33b381a24bf6a9689017e
nowgnas <dev.nowgnas@gmail.com>
2023-11-02 19:56:56 +0900

    fix: 배송 상태 조회 수정

--- src/main/java/com/lotdiz/projectservice/dto/DeliveryStatusOfFundingDto.java
+++ src/main/java/com/lotdiz/projectservice/dto/DeliveryStatusOfFundingDto.java
@@ -13,5 +13,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class DeliveryStatusOfFundingDto {
   private Long fundingId;
-  private DeliveryStatus deliveryStatus;
+  private String deliveryStatus;
 }
